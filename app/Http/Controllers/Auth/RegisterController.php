c781731c180349203d410801223aeb2d2e99cc00
brnaldomesi <brnaldomesi@yandex.com>
2019-10-27 22:53:30 +0900

    Change redirectTo after Register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/clientAdmin/feeds';
 
     /**
      * Create a new controller instance.
