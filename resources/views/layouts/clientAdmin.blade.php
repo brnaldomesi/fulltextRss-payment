235f7213f808479dcf378ec06248bb8ed8ecfc12
brnaldomesi <brnaldomesi1@gmail.com>
2019-10-27 21:05:49 -0300

    Admin page layout

--- resources/views/layouts/clientAdmin.blade.php
+++ resources/views/layouts/clientAdmin.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,41 +20,113 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand text-success font-weight-bold" href="#">
-                    {{ __('All-in-one RSS Content Engine & Big Data') }}
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm navbar-client-admin">
+            <div class="container-fluid">
+                <a class="navbar-brand text-white" href="#">
+                    {{ __('FetchRSSFeeds.COM') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse ml-1" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                      <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
-                      </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-grey" href="#">
+                                <i class="fa fa-tachometer"></i>{{ __('Admin') }}
+                            </a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-grey" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <i class="fa fa-users"></i>
+                                {{ __('Team Members') }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" onclick="">
+                                    <i class="fa fa-user-plus"></i>
+                                    {{ __('Add a new team member') }}
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" onclick="">
+                                    <i class="fa fa-street-view"></i>
+                                    {{ __('Manage team members') }}
+                                </a>
+
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-grey" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <i class="fa fa-rss"></i>
+                                {{ __('News Feed') }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" onclick="">
+                                    <i class="fa fa-plus-square"></i>
+                                    {{ __('Create News Feed Channel') }}
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" onclick="">
+                                    <i class="fa fa-th-list"></i>
+                                    {{ __('Manage News Feed Channels') }}
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" onclick="">
+                                    <i class="fa fa-cloud-download"></i>
+                                    {{ __('Download Full text RSS OPML') }}
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" onclick="">
+                                    <i class="fa fa-link"></i>
+                                    {{ __('Show me the master RSS Feed') }}
+                                </a>
+
+                            </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-grey" href="#">
+                                <i class="fa fa-pencil-square-o"></i>{{ __('Edit sender credentials') }}
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-grey" href="#">
+                                <i class="fa fa-line-chart"></i>{{ __('Statistics') }}
+                            </a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <i class="fa fa-user"></i>
                                 {{ Auth::user()->email }} <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                   document.getElementById('logout-form').submit();">
+                                    <i class="fa fa-sign-out fa-rotate-180"></i>
                                     {{ __('Logout') }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
                                     @csrf
                                 </form>
                             </div>
@@ -64,8 +137,31 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="col-2">
+                        <div class="well sidebar-nav">
+                            <ul class="nav nav-list left-menu">
+                                <li class="nav-header"><i class="fa fa-tachometer fa-2x"></i> Administration</li>
+                                <li><a href="#"><i class="fa fa-users"></i> Team Members</a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i> News Feed Channels</a></li>
+                                <li class="active"><a href="#"><i class="fa fa-pencil-square-o"></i> Sender
+                                        Credentials</a></li>
+                                <li class="nav-header"><i class="fa fa-line-chart fa-2x"></i> Statistics</li>
+                                <li><a href="#"><i class="fa fa-area-chart"></i> Account Statistics</a></li>
+                                <li><a href="#"><i class="fa fa-bar-chart"></i> Member Statistics</a></li>
+                                <li class="nav-header"><i class="fa fa-user fa-2x"></i> Profile</li>
+                                <li><a href="#"><i class="fa fa-sign-out fa-rotate-180"></i> Logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-10">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
+
 </html>
