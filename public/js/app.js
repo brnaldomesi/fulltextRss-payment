829addc70cd07953e96f9a90eea37cbbf5e6b805
brnaldomesi <brnaldomesi@yandex.com>
2019-10-27 21:05:49 -0300

    Admin page layout

--- public/js/app.js
+++ public/js/app.js
@@ -36985,8 +36985,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Task\yan-huang\FetchRSSFeeds\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Task\yan-huang\FetchRSSFeeds\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\Projects\FetchRSSFeeds\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\Projects\FetchRSSFeeds\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
